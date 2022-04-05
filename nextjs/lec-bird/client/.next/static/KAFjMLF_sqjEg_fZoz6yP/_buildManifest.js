c44e32dad7b95368c56202e4c33aed2b5bf57a8f
dosimpact <ypd03008@gmail.com>
2022-04-05 23:57:01 +0900

    refactor

--- nextjs/lec-bird/client/.next/static/KAFjMLF_sqjEg_fZoz6yP/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-09e3a49b5217573c.js"],"/_error":["static/chunks/pages/_error-924e2bedcb115c0e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
